911bdd6a437e9faa7ea5c048f6c3a08f55bdb19c
sagar samvedi <sagarsamvedi31@gmail.com>
2023-11-27 16:54:10 +0530

    Started learning react

---
+++ Day4_React/volcanus-react/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <h1>Hello, Welcome to Home Page</h1>
+      <h3>Math.floor(5/2)</h3>
+    </div>
+    </>
+  )
+}
+
+export default App
