babb28050d577911bd16a5ec53ddb09fbd2ea88b
sagar samvedi <sagarsamvedi31@gmail.com>
2023-12-04 16:29:52 +0530

    Learned Form Submit using preventDefault()

--- Day4_React/volcanus-react/src/components/MultipleInput.jsx
+++ Day4_React/volcanus-react/src/components/MultipleInput.jsx
@@ -10,9 +10,18 @@ const MultipleInput = () => {
     const handleSubmit = (e) => {
         setFormData({...formData,[e.target.name]:[e.target.value]})
     }
-    console.log(formData.name);
-    console.log(formData.email);
-    console.log(formData.password);
+    const submitHandler = (e) =>{
+        e.preventDefault();
+        alert("Your Form is Submitted");
+        setFormData({
+            name:'',
+            email:'',
+            password:''
+        })
+    }
+    // console.log(formData.name);
+    // console.log(formData.email);
+    // console.log(formData.password);
   return (
     <>
 
@@ -22,14 +31,18 @@ const MultipleInput = () => {
             Let's Learn <span> Multiple Form Handling</span> In React
           </h2>
         </div>
+        <form onSubmit={submitHandler}>
+
         <input  name = 'name'type="text" value={formData.name}placeholder="Enter Name" onChange={handleSubmit}/>
 
 
         <input  name = 'email' type="email" value={formData.email}placeholder="Enter Email" onChange={handleSubmit}/>
         
         
-        <input name = 'password' type="password" value={formData.age}placeholder="Enter Password" onChange={handleSubmit}/>
+        <input name = 'password' type="password" value={formData.age} placeholder="Enter Password" onChange={handleSubmit}/>
+
         <button>Submit</button>
+        </form>
       </div>
         
       
