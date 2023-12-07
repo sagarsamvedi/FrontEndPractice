3389809afffe072ee525f6ecd9131cbe584be1b3
sagar samvedi <sagarsamvedi31@gmail.com>
2023-12-07 15:40:09 +0530

    Day_13 Learned Conditional Rendering and Searching Product using Search Bar

--- Day4_React/volcanus-react/src/components/Routing.jsx
+++ Day4_React/volcanus-react/src/components/Routing.jsx
@@ -1,14 +1,18 @@
 import {Link} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 const Routing = () => {
+  const location = useLocation()
   return (
     <>
-    <div className="navbar">
-        <Link to={'/'}>Home</Link>
-        <Link to={'/about'}>About</Link>
-        <Link to={'/career'}>Career</Link>
-        <Link to={'/contact'}>Contact</Link>
+    {
+      <div className="navbar">
+        {location.pathname !== '/' && <Link to="/">Products</Link>}
+        {location.pathname !== '/about' && <Link to="/about">About</Link>}
+        {location.pathname !== '/career' && <Link to="/career">Career</Link>}
+        {location.pathname !== '/contact' && <Link to="/contact">Contact</Link>}
     </div>
+    }
     </>
     
   )
